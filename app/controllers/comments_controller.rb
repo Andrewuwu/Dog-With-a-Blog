01f573e3676adf8c5a93641bd40f3bacc2cd2181
Andrew Mao <andrewmao@Andrews-MacBook-Pro.local>
2021-07-12 14:05:11 -0500

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "pass", password: "password", except: :create
+
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+
+    def edit
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+    end 
+
+    def update
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end
+
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
+    
+    private
+    def comment_params
+        params.require(:comment).permit(:commenter, :body, :status)
+    end
+end
+
+
